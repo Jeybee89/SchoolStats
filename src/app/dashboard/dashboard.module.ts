84d3b0de95552b9598de857302f4b3558201549d
nick <nelson.nikolas@gmail.com>
2019-01-28 21:01:23 +0000

    adding angular material data table

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,22 +4,29 @@ import {DashboardComponent} from "./dashboard.component";
 import { TableOfSchoolsComponent } from './table-of-schools/table-of-schools.component';
 import {MaterialModule} from "../material.module";
 import {SchooldataService} from "../services/schooldata.service";
+import { SchoolDataTableComponent } from './school-data-table/school-data-table.component';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    TableOfSchoolsComponent
+    TableOfSchoolsComponent,
+    SchoolDataTableComponent
   ],
 
   exports: [
     DashboardComponent,
     TableOfSchoolsComponent,
+    SchoolDataTableComponent
 
   ],
 
   imports: [
     CommonModule,
     MaterialModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
 
   ],
   providers: [SchooldataService],
--- src/app/dashboard/table-of-schools/table-of-schools.component.ts
+++ src/app/dashboard/table-of-schools/table-of-schools.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSort, MatTableDataSource} from "@angular/material";
 import {SchooldataService} from "../../services/schooldata.service";
+import {School} from "../../model/school";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-table-of-schools',
@@ -9,12 +11,14 @@ import {SchooldataService} from "../../services/schooldata.service";
 })
 
 export class TableOfSchoolsComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  schoolData = {};
+  schoolData: any;
 
-@ViewChild(MatSort) sort: MatSort;
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  displayedColumns: string[] = ['name'];
+  dataSource = new MatTableDataSource();
 
   constructor(private schoolService: SchooldataService) {
 
@@ -23,30 +27,12 @@ export class TableOfSchoolsComponent implements OnInit {
 
   ngOnInit() {
     this.schoolService.getSchool().subscribe(res => {
-      this.schoolData = res
+      this.dataSource.data = new Array(res);
+      console.log(this.dataSource.data);
     });
     this.dataSource.sort = this.sort;
   }
 
 
 }
-let ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
